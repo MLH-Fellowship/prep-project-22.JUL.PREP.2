efcd262367605414cc56af039296053f879ce73c
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-07-21 20:09:51 +0530

    WIP

--- src/screen/trip.js
+++ src/screen/trip.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Fab } from "../Components/common/Fab";
+import { PATHS, useRouter } from "../Components/route";
 
 export function Trip() {
-  return <>Yo</>;
+  const router = useRouter();
+  const navigateToHome = () => router.push(PATHS.HOME);
+
+  return (
+    <>
+      <Fab onClick={navigateToHome} icon="thermostat">
+        Weather
+      </Fab>
+    </>
+  );
 }
 
 export default Trip;
