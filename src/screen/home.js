de4497e8902a8fe553bc0595f70b2d64f4968e47
Arpit Bhalla <55053424+arpitBhalla@users.noreply.github.com>
2022-07-25 12:24:15 +0530

    WIP

--- src/screen/home.js
+++ src/screen/home.js
@@ -3,6 +3,8 @@ import "./home.css";
 import { Fab } from "../Components/common/Fab";
 import Box from "../Components/Box";
 import logo from "../assets/logo.png";
+import Forecast from "../forecast/Forecast.js";
+import Search from "../Search";
 
 const navigateToTrip = () => (window.location.href = "/trip");
 
@@ -11,54 +13,71 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [city, setCity] = useState("New York City");
   const [results, setResults] = useState(null);
+  const [forecast, setForecast] = useState(null);
+  const [generic, setGeneric] = useState("app");
+  const [notfound, setFlag] = useState(false);
+
+  console.log(1, results);
+  const fetchWeather = (url) => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.cod !== "200") {
+          setIsLoaded(false);
+          console.log("hah");
+          if (result["cod"] == "404") {
+            setIsLoaded(true);
+            setFlag(true);
+          }
+          return null;
+        }
+        console.log("yo");
+
+        let hourlyForecast = [];
+        result.list.forEach((fc) => {
+          hourlyForecast.push({
+            current_temp: fc.main.temp,
+            condition: fc.weather[0].description,
+            date: new Date(fc.dt * 1000),
+            feels_like: fc.main.feels_like,
+            temperature: {
+              minimum: fc.main.temp_min,
+              maximum: fc.main.temp_max,
+            },
+            icon: fc.weather[0].icon,
+            windspeed: fc.wind.speed,
+            humidity: fc.main.humidity,
+          });
+        });
+        setForecast(hourlyForecast);
+        setIsLoaded(true);
+        setResults(result);
+        document.body.classList = result.list[0].weather[0].main;
+      })
+      .catch((error) => {
+        setIsLoaded(true);
+        setError(error);
+      });
+  };
 
   useEffect(() => {
     document.body.classList.add("app");
     document.title = "Weather";
-  }, []);
-
-  useEffect(() => {
     window.navigator.geolocation.getCurrentPosition((position) => {
-      fetch(
+      fetchWeather(
         `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${process.env.REACT_APP_APIKEY}`
-      )
-        .then((res) => res.json())
-        .then((result) => {
-          setIsLoaded(true);
-          setCity(`${result.name}, ${result.sys.country}`);
-          setResults(results);
-        })
-        .catch((err) => {
-          setIsLoaded(false);
-          setError(err);
-        });
+      );
     });
   }, []);
 
   useEffect(() => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
+    fetchWeather(
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
         city +
         "&units=metric" +
         "&appid=" +
         process.env.REACT_APP_APIKEY
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          if (result["cod"] !== 200) {
-            setIsLoaded(false);
-          } else {
-            setIsLoaded(true);
-            setResults(result);
-            document.body.classList = result.weather[0].main;
-          }
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+    );
   }, [city]);
 
   if (error) {
@@ -72,17 +91,11 @@ function App() {
         </div>
         <main>
           <h2>Enter a city below 👇</h2>
-          <input
-            type="text"
-            value={city}
-            onChange={(event) => {
-              setCity(event.target.value);
-            }}
-          />
+          <Search setCity={setCity} />
           <div className="Results">
             {!isLoaded && <h2>Loading...</h2>}
-            {console.log(results)}
-            {isLoaded && results && (
+
+            {/* {isLoaded && results && (
               <>
                 <h3>{results.weather[0].main}</h3>
                 <p>Feels like {results.main.feels_like}°C</p>
@@ -92,11 +105,17 @@ function App() {
                   </p>
                 </i>
               </>
+            )} */}
+            {isLoaded && forecast && (
+              <>
+                <Forecast hourlyForecast={forecast} />
+              </>
             )}
           </div>
           <p className="required-things-heading">Things you should carry 🎒</p>
-          {isLoaded && results && <Box weather={results.weather[0].main} />}
-
+          {isLoaded && results && (
+            <Box weather={results.list[0].weather[0].main} />
+          )}
           <Fab icon={"airplane_ticket"} onClick={navigateToTrip}>
             Plan Trip
           </Fab>
