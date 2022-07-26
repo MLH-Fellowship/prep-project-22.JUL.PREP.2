b963ea9e935a81aee04536c3e89ed48bf881149f
Ruchita Gosavi <ruchigosavi10@gmail.com>
2022-07-26 16:39:50 +0530

    Use xid to get data about the location

--- src/Components/Attractions.js
+++ src/Components/Attractions.js
@@ -6,9 +6,14 @@ const Attractions = (results) => {
 	const city = locationData.city.name;
 	const lng = locationData.city.coord.lon;
 	const lat = locationData.city.coord.lat;
-
+	
 	useEffect(() => {
 		fetch(`https://api.opentripmap.com/0.1/en/places/radius?radius=1000&lon=${lng}&lat=${lat}9&rate=2&format=json&limit=15&apikey=5ae2e3f221c38a28845f05b6c06a2c73eb6f2499d3b1881719d6dd39`)
+			.then(res => res.json())
+			.then(data => {
+				const xid = data[0].xid;
+				return fetch(`https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=5ae2e3f221c38a28845f05b6c06a2c73eb6f2499d3b1881719d6dd39`);
+			})
 			.then(res => res.json())
 			.then(data => {
 				console.log(data);
